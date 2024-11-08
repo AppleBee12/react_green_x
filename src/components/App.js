536f2e40b06c331fb75fc61bd3556170110f366e
AppleBee12 <mieldeluna@naver.com>
2024-11-08 12:24:18 +0900

    Auth Service 구글,깃헙,이메일 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { authService } from "../firebase";
 import AppRouter from './Router';
+//console.log(authService);
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn ] = useState(false);
   return (
-    <div>
-      hehe
-      <AppRouter/>
-    </div>
+    <AppRouter isLoggedIn={isLoggedIn}/>
   );
 }
 
