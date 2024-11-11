8bf7f1b152bf04168f5bad8b9d347ec722aa1a02
AppleBee12 <mieldeluna@naver.com>
2024-11-11 12:03:41 +0900

    isOwner

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import Form from "react-bootstrap/Form";
-import ListGroup from 'react-bootstrap/ListGroup';
+import React from "react";
+import ListGroup from "react-bootstrap/ListGroup";
+import Button from "react-bootstrap/Button";
 
-const Nav = ({commentObj})=>{
-  return(
-    <ListGroup.Item>{commentObj.comment}</ListGroup.Item>
-  )
-}
+const Nav = ({ commentObj, isOwner }) => {
+  return (
+    <ListGroup.Item>
+      <div className='d-flex justify-content-between'>
+        {commentObj.comment}
+        {isOwner &&
+        
+        <div className='d-flex gap-1'>
+          <Button variant="outline-secondary" size="sm">
+            수정
+          </Button>
+          <Button variant="outline-danger" size="sm">
+            삭제
+          </Button>
+        </div>
+        
+        }
+        
+      </div>
+    </ListGroup.Item>
+  );
+};
 export default Nav;
