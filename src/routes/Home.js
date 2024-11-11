aabf479fdcfda0592390c8c20c8888f9e376c603
AppleBee12 <mieldeluna@naver.com>
2024-11-11 10:18:27 +0900

    addDoc로 firebase에 문서추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import  Button  from 'react-bootstrap/Button';
+import { db } from '../firebase';
+import { addDoc, collection, updateDoc, serverTimestamp } from "firebase/firestore"; 
 
 
 const Home = ()=>{
@@ -11,16 +13,30 @@ const Home = ()=>{
     const {target:{value}} = e;
     setComment(value);
   }
-  const onSubmit = (e)=>{
+
+  //await가 있으려면 위에 async가 있어야해요
+  const onSubmit = async (e)=>{
     e.preventDefault();
+    console.log(comment, '실행');
+
+    try {
+      const docRef = await addDoc(collection(db, "comments"), {//comments = firebase컬렉션 이름
+        comment:comment,
+        date:serverTimestamp()
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+    
   }
   return(
     <div className="container">
     <Form onSubmit={onSubmit}>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Control type="email" onChange={onChange} placeholder="글을 입력해주세요" />
+      <Form.Group className="mb-3" controlId="comment">
+        <Form.Control type="text" onChange={onChange} placeholder="글을 입력해주세요" />
       </Form.Group>
-      <Button variant="outline-primary">입력</Button>
+      <Button variant="outline-primary" type="submit">입력</Button>
     </Form>
     </div>
   )
