04d57e9c375b8c1ffe210b963ec306c7104f7964
AppleBee12 <mieldeluna@naver.com>
2024-11-12 11:52:37 +0900

    프로필 사진, 필수사항 변경

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,34 +1,103 @@
-import React from "react";
-import { getAuth, signOut } from "firebase/auth";
+import React, { useEffect, useState } from "react";
+import { getAuth, signOut, onAuthStateChanged, updateProfile } from "firebase/auth";
 import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
+import ListGroup from "react-bootstrap/ListGroup";
 import { useNavigate } from "react-router-dom";
+import { db } from "../firebase";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { collection, query, orderBy, limit } from "firebase/firestore";
+import{authService} from '../firebase';
+import Comment from "../components/Comment";
+console.log(authService);
 
 const Profile = () => {
+  //console.log(userObj);
+  const user = authService.currentUser;
+
+  const profilePath = `${process.env.PUBLIC_URL}/profile_icon.svg`
+  const [profile, setProfile] = useState(profilePath);
+  const [comments, setComments] = useState([]); // 조회된 글 배열
   const navigate = useNavigate();
+
+  const getComments = async () => {
+    
+    const q = query(collection(db, "comments"), where("uid", "==", "user.uid")), orderBy("date","desc"), limit(5);
+    const querySnapshot = await getDocs(q);
+    const commentArr = querySnapshot.docs.map(o)
+    
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      const commentObj = {
+        ...doc.data(),
+        id:doc.id
+        }
+        setComments((prev=>[commentObj, ...prev]));
+       
+    });
+    */
+    //const commentArr = querySnapshot.docs.map(doc=>{return{...doc.data(), id:doc.id}})
+    
+  };
+
+
+
   const onLogOutClick = () => {
     const auth = getAuth();
     signOut(auth)
       .then(() => {
         // Sign-out successful.
-        alert('로그아웃 되었습니다.');
-        navigate ('/');
+        alert("로그아웃 되었습니다.");
+        navigate("/");
       })
       .catch((error) => {
         // An error happened.
         alert(error);
       });
   };
+
+  const updateLogo = async(e)=>{
+
+    const file = e.target.files[0];
+    const storage = getStorage();//파일 업로드 관련 함수 초기화
+    const storageRef = ref(storage, `profile/ ${user.uid}`);//파일이 저장될 위치와 이름
+    const result = await uploadBytes(storageRef, file);//업로드 후 결과를 result에 할당
+    const profileURL = await getDownloadURL(result.ref);//업로드 된 이미지의 절대 경로
+    setProfile(profileURL)//새 이미지로 프로필 교체
+
+    const auth = getAuth();
+      await updateProfile(auth.currentUser, {
+        photoURL: profile.URL
+      });
+  }
+  useEffect(()=>{
+    user.photoURL !== null && setProfile(user.photoURL)
+
+  },[])//최초 한번 실행, 값이 변경되면 다시 실행
+
   return (
     <div className="container">
       <h1>Profile Page</h1>
       <div className="profile">
-        <img src={`${process.env.PUBLIC_URL}/profile_icon.svg`} alt="" />
+        <img src={profile} alt="" />
       </div>
-          <input type="file" classNmae="hidden" accept="image/*" name="profile" id="profile" />
-          <Button variant="primary" type="button">프로필 이미지 변경</Button>
-          <hr/>
-      <Button variant="primary"onClick={onLogOutClick}>로그아웃</Button>
+      <input
+        type="file"  className="hidden"   accept="image/*"
+        name="profile"  id="profile"   onChange={updateLogo}
+      />
+      <div className="d-flex gap-1 mt-3">
+      <label className="btn btn-primary" htmlFor='profile'>프로필 이미지 변경</label>
+      
+      <Button variant="primary" onClick={onLogOutClick}>
+        로그아웃
+      </Button>
+      </div>
+      <h3>My Comment List</h3>
+
+      <ListGroup>
+            {comment.map(item => 
+              <Comment key={item.id} commentObj={item} isOwner={true} />
+            )}
+      </ListGroup>
     </div>
   );
 };
